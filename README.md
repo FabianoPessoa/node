154b21d1487a164d26b6c7c1aec915cf00ba5a7e
FabianoPessoa <fabiano.pessoa30@gmail.com>
2022-05-05 10:19:56 -0300

    Deu Problema no contrutor

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # node
 estudo do Node.JS
+# Criado
+$ npm init
+	Criado package.json
+Criado:
+	index.js
+	person.js
+
+No 
+$ node index.js
+Deu problema com construtor
---
+++ person.js
@@ -0,0 +1,10 @@
+class Person{
+	constructor(Person){  
+		this.name= name;
+		}
+		sayMyName(){
+			return `My name is ${this.name}!`;
+			}
+}
+
+module.export = Person;
